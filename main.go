e14613e4be425ee0de78a59bf0b9a6602612122b
Karl-Ludwig Reinhard <reinhard@synyx.de>
2020-02-29 09:24:04 +0100

    Added writing single register operation

--- main.go
+++ main.go
@@ -54,19 +54,27 @@ func main() {
 	case "writeMultipleCoils":
 		resp, err = writeMultipleCoils(client, *address, valuesAsString)
 		break
+	case "writeSingleRegister":
+		resp, err = writeSingleRegisters(client, *address, valuesAsString)
+		break
 	case "readCoils":
 		resp, err = readCoils(client, *address, *size)
 	default:
 		fmt.Println("Unknown operation - aborting")
-		return
-	}
+		fmt.Println("Known operations:")
+		fmt.Println(" writeSingleCoil")
+		fmt.Println(" writeMultipleRegisters")
+	 	fmt.Println(" writeMultipleCoils")
+	 	fmt.Println(" writeSingleRegister")
+	 	fmt.Println(" readCoils")
+	 }
 	fmt.Println("response", resp)
 	if err != nil {
 		fmt.Println("err", err)
 	}
 }
 
-func initConnection(host string, port int) (modbus.Client, error){
+func initConnection(host string, port int) (modbus.Client, error) {
 	hostPort := host + ":" + strconv.Itoa(port)
 	handler := modbus.NewTCPClientHandler(hostPort)
 	err := handler.Connect()
@@ -91,7 +99,7 @@ func convertStringArrayToInt(arrayToConvert []string) (int, error) {
 	return ret, nil
 }
 
-func writeSingleCoil(client modbus.Client, address int, valueAsString []string) ([]byte, error){
+func writeSingleCoil(client modbus.Client, address int, valueAsString []string) ([]byte, error) {
 	values, err := convertToByteArray(valueAsString)
 	if err != nil {
 		return nil, err
@@ -102,28 +110,55 @@ func writeSingleCoil(client modbus.Client, address int, valueAsString []string) 
 	return client.WriteSingleCoil(uint16(address), 0x0000)
 }
 
+func convertUInt16TArray(values []string, bitsize int) ([]byte, error) {
+	var valuesAsInt []uint64
+	for _, value := range values {
+		parsedValue, err := strconv.ParseUint(value, 10, bitsize)
+		if err != nil {
+			return nil, err
+		}
+		valuesAsInt = append(valuesAsInt, parsedValue)
+	}
+
+	byteBuffer := new(bytes.Buffer)
+	for _, valueAsInt := range valuesAsInt {
+		err := binary.Write(byteBuffer, binary.BigEndian, uint16(valueAsInt))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return byteBuffer.Bytes(), nil
+}
+
 func writeMultipleRegisters(client modbus.Client, address int, values []string) ([]byte, error) {
-	valuesBytes, err := convertToByteArray(values)
+	valuesBytes, err := convertUInt16TArray(values, 16)
 	if err != nil {
 		return nil, err
 	}
-	return client.WriteMultipleRegisters(uint16(address), uint16(len(values)), valuesBytes)
+	return client.WriteMultipleRegisters(uint16(address), uint16(len(valuesBytes)/2), valuesBytes)
 }
 
+func writeSingleRegisters(client modbus.Client, address int, values []string) ([]byte, error) {
+	valuesBytes, err := convertUInt16TArray(values, 16)
+	if err != nil {
+		return nil, err
+	}
+	return client.WriteMultipleRegisters(uint16(address), uint16(len(valuesBytes)/2), valuesBytes)
+}
 
-func reverse (a []byte) []byte {
-	for i := len(a)/2-1; i >= 0; i-- {
-		opp := len(a)-1-i
+func reverse(a []byte) []byte {
+	for i := len(a)/2 - 1; i >= 0; i-- {
+		opp := len(a) - 1 - i
 		a[i], a[opp] = a[opp], a[i]
 	}
 	return a
 }
 
 func getLen(length int) int {
-	return int(math.Ceil(float64(length)/8.0))
+	return int(math.Ceil(float64(length) / 8.0))
 }
 
-func writeMultipleCoils(client modbus.Client, address int, values []string) ([]byte, error){
+func writeMultipleCoils(client modbus.Client, address int, values []string) ([]byte, error) {
 	valuesAsInt, err := convertStringArrayToInt(values)
 	if err != nil {
 		return nil, err
