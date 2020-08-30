cbe2ee61aff2eeebc93671b508b24c6907159037
Karl-Ludwig Reinhard <reinhard@synyx.de>
2020-08-30 10:25:24 +0200

    Fixed bug regrding writing single register
    * There was a hardcoded value instead of using parameterized value

--- main.go
+++ main.go
@@ -143,12 +143,12 @@ func writeMultipleRegisters(client modbus.Client, address int, values []string) 
 }
 
 func writeSingleRegisters(client modbus.Client, address int, values []string) ([]byte, error) {
-	////valuesBytes, err := convertUInt16TArray(values, 16)
-	//if err != nil {
-	//	return nil, err
-	//}
-	return client.WriteSingleRegister(uint16(address), uint16(0))
-	return client.WriteSingleRegister(uint16(address), uint16(0))
+	valuesBytes, err := convertUInt16TArray(values, 16)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("writing", valuesBytes)
+	return client.WriteSingleRegister(uint16(address), uint16(valuesBytes[1]))
 }
 
 func reverse(a []byte) []byte {
